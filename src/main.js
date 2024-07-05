baee453cb40b1fa02a1b93d2d56365af7ad0cf95
Denys Bystryi <bystryydenis211999@gmail.com>
2024-07-05 15:34:50 +0300

    installing libraries

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Your API key: 44781462-ae4aaccc0a5ec19c0259ffb3b
+
+// q =  — слово для пошуку. Те, що буде вводити користувач.
+//— тип зображення. Потрібні тільки фотографії, тому постав значення photo.//
+//орієнтація фотографії. Постав значення horizontal.
+//safesearch — фільтр за віком. Постав значення true.
+const apiKey = '44781462-ae4aaccc0a5ec19c0259ffb3b';
+const imageType = photo;
+const orientation = horizontal;
+const safeSearch = true;
