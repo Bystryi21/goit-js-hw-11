6fffb88eb162a3796fe64e306264c66c299dc16b
Denys Bystryi <bystryydenis211999@gmail.com>
2024-07-06 22:12:53 +0300

    .

--- src/main.js
+++ src/main.js
@@ -1,64 +1,18 @@
-// Your API key: 44781462-ae4aaccc0a5ec19c0259ffb3b
-
-// q =  — слово для пошуку. Те, що буде вводити користувач.
-//— тип зображення. Потрібні тільки фотографії, тому постав значення photo.//
-//орієнтація фотографії. Постав значення horizontal.
-//safesearch — фільтр за віком. Постав значення true.
-//
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-//
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-//
-//
+import { fetchImage } from './js/pixabay-api';
+import { createModalWindow } from './js/render-functions';
+
 const loader = document.querySelector('#loader');
 const form = document.querySelector('#search-form');
-const api = 'https://pixabay.com/api/';
-const apiKey = '44781462-ae4aaccc0a5ec19c0259ffb3b';
-const imageType = 'photo';
-const orientation = ' horizontal';
-const safeSearch = true;
 const gallery = document.querySelector('.gallery');
 
 let lightbox = new SimpleLightbox('.gallery a', {
-  cationsData: 'alt',
+  captionsData: 'alt',
   captionDelay: 250,
 });
-
-function fetchImage(q) {
-  const url = `${api}?key=${apiKey}&q=${encodeURIComponent(
-    q
-  )}&image_type=${imageType}&orientation=${orientation}&safesearch=${safeSearch}`;
-  loader.style.display = 'block';
-
-  return fetch(url)
-    .then(res => {
-      loader.style.display = 'none';
-      if (!res.ok) throw new Error('Network response was not ok');
-      return res.json();
-    })
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          title: 'Error',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-        return [];
-      }
-      return data.hits;
-    })
-    .catch(error => {
-      loader.style.display = 'none';
-      console.error('Fetching error: ', error);
-      iziToast.error({
-        title: 'Error',
-        message: 'Failed to fetch images. Please try again later.',
-      });
-      return [];
-    });
-}
 
 form.addEventListener('submit', function (evt) {
   evt.preventDefault();
@@ -72,31 +26,27 @@ form.addEventListener('submit', function (evt) {
     return;
   }
 
-  fetchImage(searchQuery).then(images => {
-    if (images.length > 0) {
-      gallery.innerHTML = '';
+  loader.style.display = 'block';
+
+  fetchImage(searchQuery)
+    .then(images => {
+      loader.style.display = 'none';
       gallery.innerHTML = createModalWindow(images);
-
-      lightbox = new SimpleLightbox('.gallery a ', {
-        captionsData: 'alt',
-        captionDelay: 250,
-      });
-
       lightbox.refresh();
-    } else {
-      iziToast.info({
-        title: 'Info',
-        message: 'No images found for your search query.',
-      });
-    }
-  });
+    })
+    .catch(error => {
+      loader.style.display = 'none';
+      if (error.message === 'No images found') {
+        iziToast.error({
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      } else {
+        iziToast.error({
+          title: 'Error',
+          message: 'Failed to fetch images. Please try again later.',
+        });
+      }
+    });
 });
-function createModalWindow(images) {
-  return images
-    .map(
-      image => `<a href="${image.largeImageURL}" data-lightbox="gallery" data-title="${image.tags}">
-        <img src="${image.webformatURL}" alt="${image.tags}" />
-      </a>`
-    )
-    .join('');
-}
